3e4dec2ab0b61ab6d6eaa34251bad6f8069c1a23
Claire Walsh <cwalsh13@gmail.com>
2022-06-23 16:33:43 +0000

    add init script to gitpod.yml

--- db-init.sql
+++ db-init.sql
@@ -1,6 +1,7 @@
 create database contacts;
 create user test password 'admin1953';
 grant all privileges on database contacts to test;
+grant all privileges on all tables in schema public to test;
 \c contacts
 create table contact (id varchar(50) unique not null, first_name varchar(50) not null, last_name varchar(50) not null);
 insert into contact(id, first_name, last_name) values ('1', 'John', 'Doe');
