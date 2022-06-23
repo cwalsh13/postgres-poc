ef26c9d49876230cc96f68e245bb2c54e186f30a
Claire Walsh <cwalsh13@gmail.com>
2022-06-23 16:57:48 +0000

    more fixes

--- db-init.sql
+++ db-init.sql
@@ -1,7 +1,7 @@
 create database contacts;
 \c contacts
+alter default privileges in schema public grant select on tables to public;
 create user test password 'admin1953';
 grant all privileges on database contacts to test;
-grant all privileges on all tables in schema public to test;
 create table contact (id varchar(50) unique not null, first_name varchar(50) not null, last_name varchar(50) not null);
 insert into contact(id, first_name, last_name) values ('1', 'John', 'Doe');
